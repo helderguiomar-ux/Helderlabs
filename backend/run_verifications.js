507e593105937d9abb8118df425715b1bef7fbf1
helderguiomar-ux <helderguiomar@gmail.com>
2026-09-12 17:33:52 +0100

    fix(prisma): adicionar generator clientRoot para manter node_modules da raiz sincronizado

--- backend/run_verifications.js
+++ backend/run_verifications.js
@@ -1,3 +1,4 @@
+require('dotenv').config({ path: __dirname + '/.env' });
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const BASE_URL = 'https://helderlabs.eu';
